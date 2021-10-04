05930b69e7429652a70d4f3961f5663bee4e42ae
asath1 <satha1@mail.broward.edu>
2021-10-03 21:17:04 -0400

    Changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = process.env.PORT; 
+var port = process.env.PORT || 80; 
 app.listen(port);
 console.log('server: ' + port);
