99c4c85493635ecb912b530627d202b8e4814503
asath1 <satha1@mail.broward.edu>
2021-10-03 20:29:47 -0400

    Fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 8000; 
+var port = process.env.PORT; 
 app.listen(port);
 console.log('server on' + port);
